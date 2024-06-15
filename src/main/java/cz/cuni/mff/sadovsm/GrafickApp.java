a52bb83f141ec01b7f0230587d4400c11a223b54
juhtyg <sadovsky11143@gmail.com>
2024-06-16 00:38:36 +0200

    Create runner for graphic version

---
+++ src/main/java/cz/cuni/mff/sadovsm/GrafickApp.java
@@ -0,0 +1,17 @@
+package cz.cuni.mff.sadovsm;
+
+import cz.cuni.mff.sadovsm.visuals.SudokuFrame;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+
+public class GrafickApp {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            SudokuFrame frame = new SudokuFrame();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
+    }
+}
