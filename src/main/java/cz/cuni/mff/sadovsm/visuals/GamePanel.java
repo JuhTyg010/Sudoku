b204cd15eae35e1d633e10f32733e1a6b4af741f
juhtyg <sadovsky11143@gmail.com>
2024-08-03 00:38:34 +0200

    removed privileges, change order for better initialization

--- src/main/java/cz/cuni/mff/sadovsm/visuals/GamePanel.java
+++ src/main/java/cz/cuni/mff/sadovsm/visuals/GamePanel.java
@@ -1,38 +1,27 @@
 package cz.cuni.mff.sadovsm.visuals;
 
-import cz.cuni.mff.sadovsm.sudoku.SudokuGenerator;
-
 import javax.swing.*;
 import java.awt.*;
-import java.util.Stack;
 
 public class GamePanel extends JPanel {
 
     private SudokuPanel sudokuPanel;
-    private static final int EMPTY_CELL = 0;
-    private int[][] grid;
-    private Stack<int[]> moves;
-    private int toComplete;
-    private boolean isUndo;
+    private JTextField messageText;
+
 
     public GamePanel(int difficulty){
         //TODO: start the game, load sudokuPanel, buttons and wisperer
-        sudokuPanel = new SudokuPanel();
+
+        messageText = new JTextField();
+        sudokuPanel = new SudokuPanel(difficulty, messageText);
         setLayout(new BorderLayout());
         add(sudokuPanel, BorderLayout.CENTER);
+        add(messageText, BorderLayout.SOUTH);
 
-        doPrefill(difficulty);
 
     }
 
-    private void doPrefill(int difficulty){
-        grid = SudokuGenerator.generateSudoku(difficulty);
-        for(int i = 0; i < 9 ; i++){
-            for(int j = 0; j < 9; j++){
-                if(grid[i][j] != EMPTY_CELL)
-                    sudokuPanel.setCellValue(i, j, grid[i][j]);
-            }
-        }
-        sudokuPanel.lockPrefilled();
-    }
+
+
+
 }
