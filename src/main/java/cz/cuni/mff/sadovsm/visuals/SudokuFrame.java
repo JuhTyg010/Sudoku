47fb235e63df60cd23347d8554aaa458c13f97e9
juhtyg <sadovsky11143@gmail.com>
2024-08-05 14:38:08 +0200

    Created JDialog for end game

--- src/main/java/cz/cuni/mff/sadovsm/visuals/SudokuFrame.java
+++ src/main/java/cz/cuni/mff/sadovsm/visuals/SudokuFrame.java
@@ -1,16 +1,18 @@
 package cz.cuni.mff.sadovsm.visuals;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 import java.awt.BorderLayout;
 
 public class SudokuFrame extends JFrame {
     private GamePanel gamePanel;
     private MenuPanel menuPanel;
+    private final int WIDTH = 600;
+    private final int HEIGHT = 400;
 
 
     public SudokuFrame() {
         setTitle("Sudoku");
-        setSize(600, 400);
+        setSize(WIDTH, HEIGHT);
         setLocationRelativeTo(null);
 
         // Create panels
@@ -23,10 +25,38 @@ public class SudokuFrame extends JFrame {
     }
 
     public void gameStart(int difficulty){
-        gamePanel = new GamePanel(difficulty);
+        gamePanel = new GamePanel(this, difficulty);
         setLayout(new BorderLayout());
         add(gamePanel, BorderLayout.CENTER);
         menuPanel.setVisible(false);
+    }
+
+    public JDialog GameEnd(){
+        JDialog endDialog = new JDialog(this, "Game end");
+
+        endDialog.setUndecorated(true);
+        endDialog.setSize(400,200);
+        endDialog.setLocationRelativeTo(null);
+
+        JLabel text = new JLabel("Congratulation");
+
+        JButton menuButton = new JButton("Go to menu");
+        menuButton.addActionListener(e -> {
+            menuPanel.setVisible(true);
+            gamePanel.setVisible(false);
+            endDialog.dispose();
+        });
+
+        JButton exitButton = new JButton("Exit");
+        exitButton.addActionListener(e -> {
+            System.exit(0);
+        });
+
+        endDialog.add(text);
+        endDialog.add(menuButton);
+        endDialog.add(exitButton);
+
+        return endDialog;
     }
 
 }
